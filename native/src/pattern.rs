658e2a037736db2f516443ea5df3a4e4c1ff7f57
Christian Swinehart <drafting@samizdat.co>
2020-08-21 11:34:56 -0400

    shader is now a shareable Rc<RefCell>
    - re-derive the shader from the original image whenever the matrix is changed

--- native/src/pattern.rs
+++ native/src/pattern.rs
@@ -2,62 +2,66 @@
 use std::rc::Rc;
 use std::cell::RefCell;
 use neon::prelude::*;
-use skia_safe::{shaders, Shader, Matrix, TileMode};
+use skia_safe::{shaders, Shader, Matrix, TileMode::{Decal, Repeat}};
 
 use crate::utils::*;
 use crate::image::{Image, JsImage};
 
 #[derive(Clone)]
 pub struct CanvasPattern{
-  shader:Option<Shader>
+  stamp: Shader,
+  shader: Rc<RefCell<Shader>>
 }
 
 impl CanvasPattern{
   pub fn shader(&self) -> Option<Shader>{
-    match &self.shader {
-      Some(shader) => Some(shader.clone()),
-      None => None
-    }
+    Some(self.shader.borrow().clone())
   }
 }
-
-use TileMode::*;
 
 declare_types! {
   pub class JsCanvasPattern for CanvasPattern {
     init(_) {
-      Ok(CanvasPattern{ shader:None })
+      Ok(CanvasPattern{
+        stamp: shaders::empty(),
+        shader: Rc::new(RefCell::new(shaders::empty()))
+      })
     }
 
     constructor(mut cx){
       let mut this = cx.this();
-
       let img = cx.argument::<JsImage>(0)?;
-      let (tx, ty) = match string_arg(&mut cx, 1, "repetition")?.as_str() {
-        "repeat" => (Repeat, Repeat),
-        "repeat-x" => (Repeat, Decal),
-        "repeat-y" => (Decal, Repeat),
-        _ => return cx.throw_error("The string did not match the expected pattern")
+      let repetition = if cx.len() > 1 && cx.argument::<JsValue>(1)?.is_a::<JsNull>(){
+        "".to_string() // null is a valid synonym for "repeat" (as is "")
+      }else{
+        string_arg(&mut cx, 1, "repetition")?
       };
 
-      let shader = cx.borrow(&img, |img| {
-        match &img.image {
-          Some(image) => Some(image.to_shader((tx, ty), None)),
-          None => None
-        }
+      let (tile_x, tile_y) = match repetition.as_str() {
+        "repeat" | "" => (Repeat, Repeat),
+        "repeat-x" => (Repeat, Decal),
+        "repeat-y" => (Decal, Repeat),
+        "no-repeat" => (Decal, Decal),
+        _ => return cx.throw_error("Unknown pattern repeat style")
+      };
+
+      cx.borrow_mut(&mut this, |mut this|{
+        cx.borrow(&img, |img| {
+          if let Some(image) = &img.image {
+            this.stamp = image.to_shader((tile_x, tile_y), None);
+            this.shader.replace(this.stamp.clone());
+          }
+        });
       });
 
-      cx.borrow_mut(&mut this, |mut this| this.shader = shader );
       Ok(None)
     }
 
     method _setTransform(mut cx){
-      let mut this = cx.this();
+      let this = cx.this();
       let matrix = matrix_arg(&mut cx, 0)?;
-      cx.borrow_mut(&mut this, |mut this| {
-        if let Some(shader) = &this.shader{
-          this.shader = Some(shader.with_local_matrix(&matrix));
-        }
+      cx.borrow(&this, |this| {
+        this.shader.replace(this.stamp.with_local_matrix(&matrix));
       });
       Ok(cx.undefined().upcast())
     }
