281ef3ef2b66a53ed685f0118e3948992255fb8d
Christian Swinehart <drafting@samizdat.co>
2021-08-19 14:42:57 -0400

    removed icu::init() call
    - as feared, not having write permission for the executable's directory is a fatal error on Windows
    - this change will cause the spurious "datafile missing" error to return (see #27)
    - fixes #35

--- src/lib.rs
+++ src/lib.rs
@@ -25,9 +25,6 @@ lazy_static! {
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
 
-  use skia_safe::shaper::icu;
-  icu::init(); // needed until https://github.com/rust-skia/rust-skia/pull/486 is merged...
-
   // -- Image -------------------------------------------------------------------------------------
 
   cx.export_function("Image_new", image::new)?;
