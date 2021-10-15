d74be6441683589dbb9f8fe71c09f8a79cfd2461
meihuanyu <763084024@qq.com>
2021-10-15 16:22:19 +0800

    fix: composite_operation invalid & ellipse add_path

--- src/context/mod.rs
+++ src/context/mod.rs
@@ -200,7 +200,7 @@ impl Context2D{
       BlendMode::DstATop | BlendMode::Src =>{
         // for blend modes that affect regions of the canvas outside of the bounds of the object
         // being drawn, create an intermediate picture before drawing to the canvas
-        let mut layer_paint = self.state.paint.clone();
+        let mut layer_paint = paint.clone();
         layer_paint.set_blend_mode(BlendMode::SrcOver);
         let mut layer_recorder = PictureRecorder::new();
         layer_recorder.begin_recording(self.bounds, None);
@@ -228,7 +228,9 @@ impl Context2D{
           if let Some(canvas) = recorder.recording_canvas() {
             canvas.save();
             canvas.set_matrix(&Matrix::new_identity().into());
-            canvas.draw_picture(&pict, None, Some(&paint));
+            let mut new_paint = Paint::default();
+            new_paint.set_blend_mode(self.state.global_composite_operation);
+            canvas.draw_picture(&pict, None, Some(&new_paint));
             canvas.restore();
           }
         }
