e4104393c081f1a9e060882371197ed522622a6d
Christian Swinehart <drafting@samizdat.co>
2024-10-16 22:15:41 -0400

    use vulkano to build headless renderer
    - upgrade ash to match version vulkano is using

--- src/gpu/vulkan.rs
+++ src/gpu/vulkan.rs
@@ -1,16 +1,21 @@
+// #![allow(unused_imports)]
 use std::ptr;
 use std::cell::RefCell;
-use std::ffi::CString;
-use std::os::raw;
+use std::sync::{Arc, Mutex};
 
-use ash::{Entry, Instance, vk};
-use ash::vk::Handle;
 use skia_safe::gpu::{self, direct_contexts, surfaces, Budgeted, SurfaceOrigin};
 use skia_safe::{ImageInfo, ISize, Surface, ColorSpace};
 
-use std::sync::{Arc, Mutex};
 use skulpin_renderer::{CoordinateSystem, Renderer, RendererBuilder};
 use skulpin_renderer::rafx::api::RafxExtents2D;
+use vulkano::{
+    VulkanLibrary, VulkanObject, Handle,
+    device::{
+        physical::{PhysicalDevice, PhysicalDeviceType}, Device, DeviceCreateInfo, 
+        Queue, QueueCreateInfo, QueueFlags,
+    },
+    instance::{Instance, InstanceCreateFlags, InstanceCreateInfo},
+};
 
 #[cfg(feature = "window")]
 use winit::{
@@ -22,7 +27,7 @@ thread_local!(static VK_CONTEXT: RefCell<Option<VulkanEngine>> = RefCell::new(No
 
 pub struct VulkanEngine {
     context: gpu::DirectContext,
-    _ash_graphics: AshGraphics,
+    _vk_graphics: VkGraphics,
 }
 
 impl VulkanEngine {
@@ -45,10 +50,10 @@ impl VulkanEngine {
     }
 
     fn new() -> Result<Self, String> {
-        let ash_graphics = unsafe { AshGraphics::new("skia-canvas") }?;
+        let vk_graphics = VkGraphics::new().unwrap();
         let context = {
             let get_proc = |of| unsafe {
-                match ash_graphics.get_proc(of) {
+                match vk_graphics.get_proc(of) {
                     Some(f) => f as _,
                     None => {
                         println!("resolve of {} failed", of.name().to_str().unwrap());
@@ -56,26 +61,24 @@ impl VulkanEngine {
                     }
                 }
             };
-
             let backend_context = unsafe {
                 gpu::vk::BackendContext::new(
-                    ash_graphics.instance.handle().as_raw() as _,
-                    ash_graphics.physical_device.as_raw() as _,
-                    ash_graphics.device.handle().as_raw() as _,
+                    vk_graphics.instance.handle().as_raw() as _,
+                    vk_graphics.physical_device.handle().as_raw() as _,
+                    vk_graphics.device.handle().as_raw() as _,
                     (
-                        ash_graphics.queue_and_index.0.as_raw() as _,
-                        ash_graphics.queue_and_index.1,
+                        vk_graphics.queue.handle().as_raw() as _,
+                        vk_graphics.queue.queue_index() as usize
                     ),
                     &get_proc,
                 )
             };
-
             direct_contexts::make_vulkan(&backend_context, None)
         }.ok_or("Failed to create Vulkan context")?;
 
         Ok(Self {
             context,
-            _ash_graphics: ash_graphics,
+            _vk_graphics: vk_graphics,
         })
     }
 
@@ -132,174 +135,103 @@ impl VulkanRenderer {
         window: &Window,
         f: F,
     ) -> Result<(), String> {
-
         let size = window.inner_size();
         let window_extents = RafxExtents2D {
             width: size.width,
             height: size.height,
         };
 
-        if let Err(e) = self.skulpin.lock().unwrap().draw(
+        self.skulpin.lock().unwrap().draw(
             window_extents,
             window.scale_factor(),
             |canvas, coords| {
                 let size = coords.window_logical_size();
                 f(canvas, LogicalSize::new(size.width as f32, size.height as f32))
+            }
+        ).map_err(|e| format!("Rendering error {:?}", e))
+    }
+}
+
+
+
+
+pub struct VkGraphics {
+    pub library: Arc<VulkanLibrary>,
+    pub instance: Arc<Instance>,
+    pub physical_device: Arc<PhysicalDevice>,
+    pub device: Arc<Device>,
+    pub queue: Arc<Queue>,
+}
+
+impl VkGraphics{
+    pub fn new() -> Result<VkGraphics, String> {
+        let library = VulkanLibrary::new().unwrap();
+
+        let instance = Instance::new(
+            Arc::clone(&library),
+            InstanceCreateInfo {
+                flags: InstanceCreateFlags::ENUMERATE_PORTABILITY,
+                ..Default::default()
+            },
+        )
+        .expect("Vulkan: Could not create instance");
+    
+        let (physical_device, queue_family_index) = instance
+            .enumerate_physical_devices()
+            .unwrap()
+            // No need for swapchain extension support.
+            .filter_map(|p| {
+                p.queue_family_properties()
+                    .iter()
+                    .position(|q| q.queue_flags.intersects(QueueFlags::GRAPHICS))
+                    .map(|i| (p, i as u32))
             })
-        {
-            Err(format!("Rendering error {:?}", e))
-        }else{
-            Ok(())
-        }
+            .min_by_key(|(p, _)| match p.properties().device_type {
+                PhysicalDeviceType::DiscreteGpu => 0,
+                PhysicalDeviceType::IntegratedGpu => 1,
+                PhysicalDeviceType::VirtualGpu => 2,
+                PhysicalDeviceType::Cpu => 3,
+                PhysicalDeviceType::Other => 4,
+                _ => 5,
+            })
+            .expect("Vulkan: No suitable physical device found");
+    
+        println!(
+            "Using device: {} (type: {:?}) on {:?}",
+            physical_device.properties().device_name,
+            physical_device.properties().device_type,
+            std::thread::current().id()
+        );
+    
+        let (device, mut queues) = Device::new(
+            physical_device.clone(),
+            DeviceCreateInfo {
+                queue_create_infos: vec![QueueCreateInfo {
+                    queue_family_index,
+                    ..Default::default()
+                }],
+                ..Default::default()
+            },
+        )
+        .expect("Vulkan: Failed to create device");
+        
+        let queue = queues.next().expect("Vulkan: Failed to create queue");
 
-
-    }
-}
-
-
-pub struct AshGraphics {
-    pub entry: Entry,
-    pub instance: Instance,
-    pub physical_device: vk::PhysicalDevice,
-    pub device: ash::Device,
-    pub queue_and_index: (vk::Queue, usize),
-}
-
-impl Drop for AshGraphics {
-    fn drop(&mut self) {
-        unsafe {
-            self.device.device_wait_idle().unwrap();
-            self.device.destroy_device(None);
-            self.instance.destroy_instance(None);
-        }
-    }
-}
-
-impl AshGraphics {
-    pub fn vulkan_version() -> Option<(usize, usize, usize)> {
-        let entry = unsafe { Entry::load() }.unwrap();
-
-        let detected_version = entry.try_enumerate_instance_version().unwrap_or(None);
-
-        detected_version.map(|ver| {
-            (
-                vk::api_version_major(ver).try_into().unwrap(),
-                vk::api_version_minor(ver).try_into().unwrap(),
-                vk::api_version_patch(ver).try_into().unwrap(),
-            )
-        })
-    }
-
-    pub unsafe fn new(app_name: &str) -> Result<AshGraphics, String> {
-        let entry = Entry::load().or(Err("Failed to load Vulkan entry"))?;
-
-        let minimum_version = vk::make_api_version(0, 1, 0, 0);
-
-        let instance: Instance = {
-            let api_version = Self::vulkan_version()
-                .map(|(major, minor, patch)| {
-                    vk::make_api_version(
-                        0,
-                        major.try_into().unwrap(),
-                        minor.try_into().unwrap(),
-                        patch.try_into().unwrap(),
-                    )
-                })
-                .unwrap_or(minimum_version);
-
-            let app_name = CString::new(app_name).unwrap();
-            let layer_names: [&CString; 0] = []; // [CString::new("VK_LAYER_LUNARG_standard_validation").unwrap()];
-            let extension_names_raw = []; // extension_names();
-
-            let app_info = vk::ApplicationInfo::builder()
-                .application_name(&app_name)
-                .application_version(0)
-                .engine_name(&app_name)
-                .engine_version(0)
-                .api_version(api_version);
-
-            let layers_names_raw: Vec<*const raw::c_char> = layer_names
-                .iter()
-                .map(|raw_name| raw_name.as_ptr())
-                .collect();
-
-            let create_info = vk::InstanceCreateInfo::builder()
-                .application_info(&app_info)
-                .enabled_layer_names(&layers_names_raw)
-                .enabled_extension_names(&extension_names_raw);
-
-            entry
-                .create_instance(&create_info, None)
-        }.or(Err("Failed to create a Vulkan instance."))?;
-
-        let (physical_device, queue_family_index) = {
-            let physical_devices = instance
-                .enumerate_physical_devices()
-                .expect("Failed to enumerate Vulkan physical devices.");
-
-            physical_devices
-                .iter()
-                .map(|physical_device| {
-                    instance
-                        .get_physical_device_queue_family_properties(*physical_device)
-                        .iter()
-                        .enumerate()
-                        .find_map(|(index, info)| {
-                            let supports_graphic =
-                                info.queue_flags.contains(vk::QueueFlags::GRAPHICS);
-                            if supports_graphic {
-                                Some((*physical_device, index))
-                            } else {
-                                None
-                            }
-                        })
-                })
-                .find_map(|v| v)
-        }.ok_or("Failed to find a Vulkan physical device.")?;
-
-        let device: ash::Device = {
-            let features = vk::PhysicalDeviceFeatures::default();
-
-            let priorities = [1.0];
-
-            let queue_info = [vk::DeviceQueueCreateInfo::builder()
-                .queue_family_index(queue_family_index as _)
-                .queue_priorities(&priorities)
-                .build()];
-
-            let device_extension_names_raw = [];
-
-            let device_create_info = vk::DeviceCreateInfo::builder()
-                .queue_create_infos(&queue_info)
-                .enabled_extension_names(&device_extension_names_raw)
-                .enabled_features(&features);
-
-            instance
-                .create_device(physical_device, &device_create_info, None)
-        }.or(Err("Failed to create Device."))?;
-
-        let queue_index: usize = 0;
-        let queue: vk::Queue = device.get_device_queue(queue_family_index as _, queue_index as _);
-
-        Ok(AshGraphics {
-            queue_and_index: (queue, queue_index),
-            device,
-            physical_device,
-            instance,
-            entry,
-        })
+        Ok(Self{ library, instance, physical_device, device, queue })
     }
 
     pub unsafe fn get_proc(&self, of: gpu::vk::GetProcOf) -> Option<unsafe extern "system" fn()> {
+        let fns = self.instance.fns();
         match of {
             gpu::vk::GetProcOf::Instance(instance, name) => {
-                let ash_instance = vk::Instance::from_raw(instance as _);
-                self.entry.get_instance_proc_addr(ash_instance, name)
+                let vk_instance = ash::vk::Instance::from_raw(instance as _);
+                self.library.get_instance_proc_addr(vk_instance, name)
             }
             gpu::vk::GetProcOf::Device(device, name) => {
-                let ash_device = vk::Device::from_raw(device as _);
-                self.instance.get_device_proc_addr(ash_device, name)
+                let vk_device = ash::vk::Device::from_raw(device as _);
+                (fns.v1_0.get_device_proc_addr)(vk_device, name)
             }
         }
     }
+
 }
