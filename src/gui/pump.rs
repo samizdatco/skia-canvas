0fc76ba79b5e293833c7a8e4d8549bcd083de497
Christian Swinehart <drafting@samizdat.co>
2024-11-28 02:03:04 -0500

    use old-style event handling so roundtrip closure can be shared

--- src/gui/pump.rs
+++ src/gui/pump.rs
@@ -5,7 +5,7 @@ use serde_json::{json, Value};
 use winit::{
     application::ApplicationHandler,
     platform::pump_events::{EventLoopExtPumpEvents, PumpStatus},
-    event::{ElementState, KeyEvent, StartCause, WindowEvent},
+    event::{ElementState, KeyEvent, StartCause, Event, WindowEvent},
     event_loop::{EventLoop, EventLoopProxy, ActiveEventLoop, ControlFlow},
     keyboard::{PhysicalKey, KeyCode},
     window::WindowId
@@ -18,58 +18,125 @@ use super::{add_event, new_proxy};
 use crate::context::page::Page;
 
 pub struct AppBundle{
-    pub app: App,
-    pub event_loop: EventLoop<CanvasEvent>,
-    pub proxy: EventLoopProxy<CanvasEvent>,
+    windows: WindowManager,
+    cadence: Cadence,
 }
 
 impl Default for AppBundle{
     fn default() -> Self {
-        let event_loop = EventLoop::with_user_event().build().unwrap();
-        event_loop.set_control_flow(ControlFlow::Wait);
-        let proxy = event_loop.create_proxy();
-        let app = App::with_proxy(proxy.clone());
-        Self{app, event_loop, proxy}
-    }
-}
-
-impl AppBundle{
-    pub fn run_cycle(&mut self) -> Value{
-        let timeout = Some(Duration::ZERO);
-        let status = self.event_loop.pump_app_events(timeout, &mut self.app);
-        self.app.get_payload()
-    }
-}
-
-pub struct App{
-    pub proxy: EventLoopProxy<CanvasEvent>,
-    windows: WindowManager,
-    cadence: Cadence,
-    payload: Value,
-}
-
-impl App{
-    fn with_proxy(proxy:EventLoopProxy<CanvasEvent>) -> Self{
         let windows = WindowManager::default();
         let cadence = Cadence::default();
-        let payload = json!({});
-        Self{proxy, windows, cadence, payload}
+        Self{windows, cadence}
     }
+}
 
-    fn initial_sync(&mut self){
-        self.payload = self.windows.get_geometry();
-        // println!("initial {:#?}", &self.payload);
-        // self.payload.push(self.windows.get_geometry());
-    }
+#[allow(deprecated)]
+impl AppBundle{
+    pub fn run_cycle<F>(&mut self, event_loop:&mut EventLoop<CanvasEvent>, mut roundtrip:F) -> Value
+        where F:FnMut(Value, &mut WindowManager) -> NeonResult<()>
+    {
+        let mut payload = json!({});
 
-    fn roundtrip(&mut self){
-        self.payload = self.windows.get_ui_changes();
-        // println!("roundtrip {:#?}", &self.payload);
-        // self.payload.push(self.windows.get_ui_changes());
-    }
+        let timeout = Some(Duration::ZERO);
+        let status = event_loop.pump_events(timeout, |event, event_loop| match event {
+            Event::WindowEvent { event:ref win_event, window_id } => {
+                self.windows.capture_ui_event(&window_id, win_event);
 
-    pub fn get_payload(&mut self) -> Value{
-        std::mem::replace(&mut self.payload, json!({}))
+                match win_event {
+                    WindowEvent::Destroyed | WindowEvent::CloseRequested => {
+                        self.windows.remove(&window_id);
+                        if self.windows.is_empty() {
+                            // quit after the last window is closed
+                            event_loop.exit();
+                        }
+                    }
+
+                    WindowEvent::KeyboardInput {
+                        event:
+                            KeyEvent {
+                                physical_key: PhysicalKey::Code(KeyCode::Escape),
+                                state: ElementState::Pressed,
+                                repeat: false,
+                                ..
+                            },
+                        ..
+                    } => {
+                        self.windows.set_fullscreen_state(&window_id, false);
+                    }
+
+                    #[cfg(target_os = "macos")]
+                    WindowEvent::Occluded(is_hidden) => {
+                        self.windows.send_event(&window_id, CanvasEvent::RedrawingSuspended(*is_hidden));
+                    }
+
+                    WindowEvent::RedrawRequested => {
+                        self.windows.send_event(&window_id, CanvasEvent::RedrawRequested);
+                    }
+
+                    WindowEvent::Resized(size) => {
+                        self.windows.send_event(&window_id, CanvasEvent::WindowResized(*size));
+                    }
+
+                    _ => {}
+                }
+            },
+
+
+            Event::UserEvent(canvas_event) => match canvas_event{
+                CanvasEvent::Open(spec, page) => {
+                    self.windows.add(event_loop, new_proxy(), spec, page);
+                    // self.initial_sync();
+                    // payload = self.windows.get_geometry();
+                    roundtrip(self.windows.get_geometry(), &mut self.windows).ok();
+                }
+                CanvasEvent::Close(token) => {
+                    self.windows.remove_by_token(&token);
+                }
+                CanvasEvent::Quit => {
+                    event_loop.exit();
+                }
+                CanvasEvent::Render => {
+                    // relay UI-driven state changes to js and render the next frame in the (active) cadence
+                    // self.roundtrip();
+                    // payload = self.windows.get_ui_changes();
+                    roundtrip(self.windows.get_ui_changes(), &mut self.windows).ok();
+                }
+                CanvasEvent::Transform(window_id, matrix) => {
+                    self.windows.use_ui_transform(&window_id, &matrix);
+                },
+                CanvasEvent::InFullscreen(window_id, is_fullscreen) => {
+                    self.windows.use_fullscreen_state(&window_id, is_fullscreen);
+                }
+                CanvasEvent::FrameRate(fps) => {
+                    self.cadence.set_frame_rate(fps)
+                }
+                _ => {}
+            },
+
+
+            Event::AboutToWait => {
+                self.windows.dispatch_events();
+
+                // when no windows have frame/draw handlers, the (inactive) cadence will never trigger
+                // a Render event, so only do a roundtrip if there are new UI events to be relayed
+                if !self.cadence.active() && self.windows.has_ui_changes() {
+                    // payload = self.windows.get_ui_changes();
+                    roundtrip(self.windows.get_ui_changes(), &mut self.windows).ok();
+                }
+
+                // delegate timing to the cadence if active, otherwise wait for ui events
+                event_loop.set_control_flow(
+                    match self.cadence.active(){
+                        true => self.cadence.on_next_frame(||{ add_event(CanvasEvent::Render) }),
+                        false => ControlFlow::Wait
+                    }
+                );
+
+            }
+            _ => {}
+        });
+
+        payload
     }
 
     pub fn update_windows(&mut self, specs:Vec<WindowSpec>, pages:Vec<Page>){
@@ -77,116 +144,6 @@ impl App{
             self.windows.update_window(spec, page)
         });
     }
-
-}
-
-impl ApplicationHandler<CanvasEvent> for App{
-    fn resumed(&mut self, event_loop:&ActiveEventLoop){
-
-    }
-
-    fn new_events(&mut self, event_loop:&ActiveEventLoop, cause:StartCause) {
-        if cause == StartCause::Init{
-            // on initial pass, do a roundtrip to sync up the Window object's state attrs:
-            // send just the initial window positions then read back all state
-            // self.initial_sync();
-        }
-        // println!("cause: {:?}", cause);
-    }
-
-    fn window_event( &mut self, event_loop:&ActiveEventLoop, window_id:WindowId, event:WindowEvent){
-        // println!("window: {:?}: {:?}", event, window_id);
-        // dbg!(&event);
-
-        // route UI events to the relevant window
-        self.windows.capture_ui_event(&window_id, &event);
-
-        // handle window lifecycle events
-        match event {
-            WindowEvent::Destroyed | WindowEvent::CloseRequested => {
-                self.windows.remove(&window_id);
-                if self.windows.is_empty() {
-                    // quit after the last window is closed
-                    event_loop.exit();
-                }
-            }
-            WindowEvent::KeyboardInput {
-                event:
-                    KeyEvent {
-                        physical_key: PhysicalKey::Code(KeyCode::Escape),
-                        state: ElementState::Pressed,
-                        repeat: false,
-                        ..
-                    },
-                ..
-            } => {
-                self.windows.set_fullscreen_state(&window_id, false);
-            }
-
-            #[cfg(target_os = "macos")]
-            WindowEvent::Occluded(is_hidden) => {
-                self.windows.send_event(&window_id, CanvasEvent::RedrawingSuspended(is_hidden));
-            }
-
-            WindowEvent::RedrawRequested => {
-                self.windows.send_event(&window_id, CanvasEvent::RedrawRequested);
-            }
-
-            WindowEvent::Resized(size) => {
-                self.windows.send_event(&window_id, CanvasEvent::WindowResized(size));
-            }
-            _ => {}
-        }
-    }
-
-    fn user_event(&mut self, event_loop:&ActiveEventLoop, event:CanvasEvent) {
-        // println!("canvas: {:?}", event);
-        match event{
-            CanvasEvent::Open(spec, page) => {
-                self.windows.add(event_loop, self.proxy.clone(), spec, page);
-                self.initial_sync();
-            }
-            CanvasEvent::Close(token) => {
-                self.windows.remove_by_token(&token);
-            }
-            CanvasEvent::Quit => {
-                event_loop.exit();
-            }
-            CanvasEvent::Render => {
-                // relay UI-driven state changes to js and render the next frame in the (active) cadence
-                self.roundtrip();
-            }
-            CanvasEvent::Transform(window_id, matrix) => {
-                self.windows.use_ui_transform(&window_id, &matrix);
-            },
-            CanvasEvent::InFullscreen(window_id, is_fullscreen) => {
-                self.windows.use_fullscreen_state(&window_id, is_fullscreen);
-            }
-            CanvasEvent::FrameRate(fps) => {
-                self.cadence.set_frame_rate(fps)
-            }
-            _ => {}
-        }
-    }
-
-    fn about_to_wait(&mut self, event_loop:&ActiveEventLoop) {
-        self.windows.dispatch_events();
-
-        // when no windows have frame/draw handlers, the (inactive) cadence will never trigger
-        // a Render event, so only do a roundtrip if there are new UI events to be relayed
-        if !self.cadence.active() && self.windows.has_ui_changes() {
-            self.roundtrip();
-        }
-
-        // delegate timing to the cadence if active, otherwise wait for ui events
-        event_loop.set_control_flow(
-            match self.cadence.active(){
-                true => self.cadence.on_next_frame(||{ self.proxy.send_event(CanvasEvent::Render).ok(); }),
-                false => ControlFlow::Wait
-            }
-        );
-    }
-
 }
 
 
