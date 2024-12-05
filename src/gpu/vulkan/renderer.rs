0fa1dca1e52ac368f36744657b6167dce8c93c87
Christian Swinehart <drafting@samizdat.co>
2024-12-05 18:48:56 -0500

    make vulkan format static

--- src/gpu/vulkan/renderer.rs
+++ src/gpu/vulkan/renderer.rs
@@ -30,7 +30,7 @@ use crate::{
     gui::event::GpuEvent,
 };
 
-use super::{supported_vulkano_formats, to_sk_format};
+use super::{VK_FORMATS, to_sk_format};
 
 
 pub struct VulkanRenderer{
@@ -119,17 +119,19 @@ impl VulkanRenderer {
                 .surface_capabilities(&surface, Default::default())
                 .unwrap();
 
-
             // choose the first device format that is on the supported list
-            let supported_formats = supported_vulkano_formats();
             let device_formats = physical_device
                 .surface_formats(&surface, Default::default())
                 .unwrap();
             let (image_format, _) = device_formats.clone()
                 .into_iter()
-                .find(|(fmt, _)| supported_formats.contains(fmt))
+                .find(|(fmt, _)| VK_FORMATS.contains(fmt))
                 .unwrap_or_else(||
-                    panic!("Vulkan: no format supported by Skia was found. Supported: {:#?} Found: {:#?}", supported_formats, device_formats)
+                    panic!(
+                        "Vulkan: no format supported by Skia was found on device.\nSupported formats: {:?}\nDevice formats: {:?}",
+                        VK_FORMATS,
+                        device_formats
+                    )
                 );
 
             Swapchain::new(
