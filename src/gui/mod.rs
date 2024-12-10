896c1708715e6b24a6a5db2b3ae96d69b87a6d1c
Christian Swinehart <drafting@samizdat.co>
2024-12-10 02:03:42 -0500

    move looping into async `App.launch()`
    - the `activate` rust method now runs a background thread that schedules winit event-loop cycles on the main node event loop
    - App.launch() returns a promise that will resolve to `undefined` when the last active window is closed
    - the 'roundtrip' callback is now registered once at startup rather than passed with every `activate` call
    - most of the roundtrip packing/unpacking now lives in the `dispatch_events` helper function

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,10 +1,15 @@
 #![allow(unused_imports)]
 #![allow(unused_variables)]
 #![allow(dead_code)]
-use neon::{prelude::*, result::Throw};
-use std::iter::zip;
+use neon::prelude::*;
+use std::{
+    sync::{Arc, OnceLock},
+    iter::zip,
+    cell::RefCell,
+    thread::sleep,
+    time::Duration,
+};
 use serde_json::Value;
-use std::cell::RefCell;
 use winit::event_loop::{EventLoop, EventLoopProxy};
 
 use crate::utils::*;
@@ -23,6 +28,7 @@ pub mod window_mgr;
 use window_mgr::WindowManager;
 
 use crate::gpu::RenderingEngine;
+use crate::context::page::Page;
 
 thread_local!(
     static APP: RefCell<App> = RefCell::new(App::default());
@@ -32,11 +38,13 @@ thread_local!(
     ));
 );
 
+static RENDER_CALLBACK: OnceLock<Arc<Root<JsFunction>>> = OnceLock::new();
+
 pub(crate) fn add_event(event: AppEvent){
     PROXY.with_borrow_mut(|proxy| proxy.send_event(event).ok() );
 }
 
-fn validate_gpu(cx:&mut FunctionContext) -> Result<(), Throw>{
+fn validate_gpu(cx:&mut FunctionContext) -> NeonResult<()>{
     // bail out if we can't draw to the screen
     if let Some(reason) = RenderingEngine::default().lacks_gpu_support(){
         cx.throw_error(reason)?
@@ -44,60 +52,98 @@ fn validate_gpu(cx:&mut FunctionContext) -> Result<(), Throw>{
     Ok(())
 }
 
-pub fn activate(mut cx: FunctionContext) -> JsResult<JsBoolean> {
-    let callback = cx.argument::<JsFunction>(1)?;
+pub fn register(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+    let callback = cx.argument::<JsFunction>(1)?.root(&mut cx);
+    RENDER_CALLBACK.get_or_init(|| Arc::new(callback));
 
-    validate_gpu(&mut cx)?;
+    Ok(cx.undefined())
+}
 
-    // closure for using the callback to relay events to js and receive updates in return
-    let roundtrip = |payload:Value, windows:Option<&mut WindowManager>| -> NeonResult<()>{
-        let is_render = windows.is_some();
-        let cx = &mut cx;
+fn dispatch_events(cx:&mut TaskContext, payload:Value, is_render:bool) -> NeonResult<Vec<(WindowSpec, Page)>>{
+    // send payload to js for event dispatch and canvas drawing
+    let mut call = match RENDER_CALLBACK.get(){
+        None => return Ok(vec![]),
+        Some(callback)=> callback.to_inner(cx).call_with(cx),
+    };
+    call.arg(cx.boolean(is_render))
+        .arg(cx.string(payload.to_string()));
 
-        // send payload to js for event dispatch and canvas drawing
-        let mut call = callback.call_with(cx);
-        call.arg(cx.boolean(is_render))
-            .arg(cx.string(payload.to_string()));
+    match is_render{
+        true => {
+            // for a full roundtrip, pass events to js then unpack updated window specs & contexts
+            let response = call.apply::<JsValue, _>(cx)?
+                .downcast::<JsArray, _>(cx).or_throw(cx)?
+                .to_vec(cx)?;
 
-        match windows{
-            Some(window_mgr) => {
-                // update window specs & page content, but only if it's time to render a new frame
-                let response = call.apply::<JsValue, _>(cx)?
-                    .downcast::<JsArray, _>(cx).or_throw(cx)?
-                    .to_vec(cx)?;
+            let specs_json = response[0].downcast::<JsString, _>(cx).or_throw(cx)?.value(cx);
+            let specs:Vec<WindowSpec> = serde_json::from_str(&specs_json)
+                .or_else(|err| cx.throw_error("Malformed response from window event handler") )?;
 
-                // unpack boxed contexts & window state objects
-                let contexts:Vec<Handle<JsValue>> = response[1].downcast::<JsArray, _>(cx).or_throw(cx)?.to_vec(cx)?;
-                let specs:Vec<WindowSpec> = serde_json::from_str(
-                    &response[0].downcast::<JsString, _>(cx).or_throw(cx)?.value(cx)
-                ).or_else(|err| cx.throw_error("Malformed response from window event handler") )?;
+            let contexts = response[1].downcast::<JsArray, _>(cx).or_throw(cx)?.to_vec(cx)?;
+            let pages = contexts.iter().map(|boxed|
+                boxed.downcast::<BoxedContext2D, _>(cx).ok()
+                    .map(|ctx| ctx.borrow().get_page())
+            );
 
-                // pass each window's new state & page data to the window manager
-                zip(contexts, specs).for_each(|(boxed_ctx, spec)| {
-                    if let Ok(ctx) = boxed_ctx.downcast::<BoxedContext2D, _>(cx){
-                        window_mgr.update_window(
-                            spec.clone(),
-                            ctx.borrow().get_page()
-                        )
-                    }
-                });
-            },
-            None => {
-                // if this is just a UI-event delivery pass, ignore the return value
-                call.exec(cx)?
-            },
+            // group spec + page pairs for each window
+            let window_state = zip(specs, pages)
+                .filter_map(|(spec, page)| page.map(|page| (spec, page) ))
+                .collect();
+
+            Ok(window_state)
         }
 
-        Ok(())
-    };
+        false => {
+            // if this is just a UI-event delivery pass, ignore the js callback's return value
+            call.exec(cx)?;
+            Ok(vec![])
+        }
+    }
+}
 
-    #[allow(deprecated)]
-    let still_running = APP.with_borrow_mut(|app| {
-        EVENT_LOOP.with_borrow_mut(|event_loop|{
-            app.activate(event_loop, roundtrip)
-        })
+pub fn activate(mut cx: FunctionContext) -> JsResult<JsPromise> {
+    validate_gpu(&mut cx)?;
+
+    let (deferred, promise) = cx.promise();
+    let channel = cx.channel();
+
+    std::thread::spawn(move || {
+        loop{
+            // schedule a callback on the node event loop
+            let keep_running = channel.send(move |mut cx| {
+
+                // define closure to relay events to js and receive canvas updates in return
+                let roundtrip = |payload:Value, windows:Option<&mut WindowManager>| -> NeonResult<()>{
+                    let window_state = dispatch_events(&mut cx, payload, windows.is_some())?;
+                    if let Some(window_mgr) = windows{
+                        for (spec, page) in window_state{
+                            window_mgr.update_window(spec, page)
+                        }
+                    }
+
+                    Ok(())
+                };
+
+                // run the winit event loop (either once or until all windows are closed depending on mode)
+                Ok(APP.with_borrow_mut(|app| {
+                    EVENT_LOOP.with_borrow_mut(|event_loop|{
+                        app.activate(event_loop, roundtrip)
+                    })
+                }))
+            }).join();
+
+            // in node-events mode, wait briefly before checking for new events
+            match keep_running{
+                Ok(true) => sleep(Duration::from_millis(1)),
+                _ => break
+            }
+        }
+
+        // resolve the promise
+        deferred.settle_with(&channel, move |mut cx| Ok(cx.undefined()) );
     });
-    Ok(cx.boolean(still_running))
+
+    Ok(promise)
 }
 
 pub fn set_rate(mut cx: FunctionContext) -> JsResult<JsNumber> {
