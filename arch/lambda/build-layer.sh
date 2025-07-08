234b0c1387f21e619b069163898f9055653a5dff
Christian Swinehart <drafting@samizdat.co>
2025-07-08 00:17:44 -0400

    tidy up build script

--- arch/lambda/build-layer.sh
+++ arch/lambda/build-layer.sh
@@ -2,26 +2,26 @@
 # see `skia-canvas-lambda.zip` recipe in Makefile for invocation details
 
 case "$(uname -p)" in
- x86_64) LAYER_ZIP="/mnt/aws-lambda-x64.zip" ;;
- aarch64) LAYER_ZIP="/mnt/aws-lambda-arm64.zip" ;;
+ x86_64) LAYER_ZIP="aws-lambda-x64.zip" ;;
+ aarch64) LAYER_ZIP="aws-lambda-arm64.zip" ;;
 esac
 
-NODE_DIR="/opt/layer/nodejs"
 LAYER_DIR="/opt/layer"
 
 yum -y install fontconfig nodejs zip git
 
-mkdir -p ${NODE_DIR} && \
-    cd  ${NODE_DIR} && \
+mkdir -p ${LAYER_DIR}/node && \
+    cd  ${LAYER_DIR}/node && \
     npm install https://github.com/samizdatco/skia-canvas
 
 mkdir -p ${LAYER_DIR}/lib && \
-    cp /lib64/libfontconfig.so* ${LAYER_DIR}/lib && \
-    cp /lib64/libfreetype.so* ${LAYER_DIR}/lib && \
-    cp /lib64/libpng16.so* ${LAYER_DIR}/lib && \
-    cp /lib64/libharfbuzz.so* ${LAYER_DIR}/lib && \
-    cp /lib64/libgraphite2.so* ${LAYER_DIR}/lib && \
-    cp /lib64/libbrotlicommon.so* ${LAYER_DIR}/lib && \
-    cp /lib64/libbrotlidec.so* ${LAYER_DIR}/lib
+    cd  ${LAYER_DIR}/lib && \
+    cp /lib64/libfontconfig.so* . && \
+    cp /lib64/libfreetype.so* . && \
+    cp /lib64/libpng16.so* . && \
+    cp /lib64/libharfbuzz.so* . && \
+    cp /lib64/libgraphite2.so* . && \
+    cp /lib64/libbrotlicommon.so* . && \
+    cp /lib64/libbrotlidec.so* .
 
-cd ${LAYER_DIR} && zip -r9 ${LAYER_ZIP} *
+cd ${LAYER_DIR} && zip -r9 /mnt/${LAYER_ZIP} *
