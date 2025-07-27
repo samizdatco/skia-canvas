60edd2eadf27095f4c8f0d2a3718b87f93285f9b
Christian Swinehart <drafting@samizdat.co>
2025-07-27 18:59:42 -0400

    pull release artifacts directly for layer build

--- arch/lambda/build-layer.sh
+++ arch/lambda/build-layer.sh
@@ -1,17 +1,28 @@
 #!/usr/bin/env bash
 
+# to create `aws-lambda-x64.zip`, run this from the project root:
+#   docker run --platform linux/amd64 -v ./arch/lambda:/opt/run -v .:/mnt amazonlinux:2023 bash /opt/run/build-layer.sh
+# to create `aws-lambda-arm64.zip`, run:
+#   docker run --platform linux/arm64 -v ./arch/lambda:/opt/run -v .:/mnt amazonlinux:2023 bash /opt/run/build-layer.sh
+
+yum -y install fontconfig nodejs zip git
+
 case "$(uname -p)" in
  x86_64) LAYER_ZIP="aws-lambda-x64.zip" ;;
  aarch64) LAYER_ZIP="aws-lambda-arm64.zip" ;;
 esac
 
-LAYER_DIR="/opt/layer"
+LAYER_DIR=/opt/layer
+PREBUILD=/opt/skia.node
+REPO=https://github.com/samizdatco/skia-canvas
 
-yum -y install fontconfig nodejs zip git
-
-mkdir -p ${LAYER_DIR}/node && \
-    cd  ${LAYER_DIR}/node && \
-    npm install https://github.com/samizdatco/skia-canvas
+mkdir -p ${LAYER_DIR}/node && cd ${LAYER_DIR}/node
+if [ -f $PREBUILD ]; then
+    npm install --ignore-scripts $REPO
+    cp $PREBUILD node_modules/skia-canvas/lib
+else
+    npm install $REPO
+fi
 
 mkdir -p ${LAYER_DIR}/lib && \
     cd  ${LAYER_DIR}/lib && \
