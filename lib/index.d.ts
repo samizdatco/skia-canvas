5c259c1d436cfda69b719ea34cbfd1bdd4ae8f35
Christian Swinehart <drafting@samizdat.co>
2026-07-26 01:46:44 -0400

    add request/cancelAnimationFrame methods to Window

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1035,6 +1035,9 @@ export class Window extends EventEmitter<{
 
   open(): void
   close(): void
+
+  requestAnimationFrame(callback: (frame: number) => void): number
+  cancelAnimationFrame(id: number): void
 }
 
 export interface App extends EventEmitter<{
